7efb032eb4e4bf372505d993d0dabc3a2729620a
siddheshm303 <siddheshm303@gmail.com>
2025-09-12 20:24:24 +0530

    MongoDB in connected

---
+++ backend/src/config/db.js
@@ -0,0 +1,20 @@
+// src/config/db.js
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const connectDB = async () => {
+  try {
+    const uri = process.env.MONGO_URI;
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
