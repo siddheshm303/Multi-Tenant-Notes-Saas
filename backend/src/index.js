625341d5cebaccd79d6e08190ed6d03ef7be1c7b
siddheshm303 <siddheshm303@gmail.com>
2025-09-15 15:58:21 +0530

    Restrict CORS to allowed origins

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
 
 
@@ -6,8 +7,28 @@ connectDB();
 
 const app = express();
 
-const cors = require('cors');
-app.use(cors());
+
+//Allowed origins (local + Vercel frontend)
+const allowedOrigins = [
+  'http://localhost:3000', // for local frontend dev
+  'https://multi-tenant-notes-saas-frontend.vercel.app' // deployed frontend
+];
+
+//Configure CORS
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+app.options("*", cors());
 
 app.use(express.json());
 
